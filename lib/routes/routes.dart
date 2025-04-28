15feba89e25ab45e8f0774f53b2859965e504992
Your Correct GitHub Username <your-correct-email@example.com>
2025-04-28 23:43:24 +0500

    set routes file

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -5,21 +5,12 @@ import '../views/homes_screen.dart';
 
 class Routes {
   static const String home = '/';
-  static const String portfolio = '/portfolio';
-  static const String services = '/services';
-
-  static const String services_description = '/services_description';
   static const String splashScreen = '/splashScreen';
-  static const String ai_learning = '/ai_learning';
 }
 
 class AppPages {
   static final List<GetPage> pages = [
     GetPage(name: Routes.home, page: () => HomesScreen()),
-    // GetPage(name: Routes.home, page: () => HomeScreen()),
-
     GetPage(name: Routes.splashScreen, page: () => SplashScreen()),
-    // GetPage(name: Routes.services, page: () => OurservicesScreen()),
-    // GetPage(name: Routes.ai_learning, page: () => AIMLScreen()),
   ];
 }
