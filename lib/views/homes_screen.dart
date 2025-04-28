90238ddb6ba65df981f9dd02da0eef63021b30d9
Your Correct GitHub Username <your-correct-email@example.com>
2025-04-28 21:16:52 +0500

    Update homes_screen

--- lib/views/homes_screen.dart
+++ lib/views/homes_screen.dart
@@ -12,9 +12,7 @@ import 'package:news_app/controller/categoriesNews.dart';
 import 'package:news_app/controller/new_view_model_controller.dart';
 import 'package:news_app/repository/news_repository.dart';
 import 'package:news_app/views/category_news.dart';
-
 import '../constant/pakistan_news_channel.dart';
-import '../controller/categoriesNews.dart';
 
 class HomesScreen extends StatefulWidget {
   const HomesScreen({super.key});
@@ -44,118 +42,125 @@ class _HomesScreenState extends State<HomesScreen> {
         SystemNavigator.pop();
         return false;
       },
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          scrolledUnderElevation: 0,
-          elevation: 0,
+      child: RefreshIndicator(
+        color: Colors.black,
+        onRefresh: () async {
+          await newsRepository.fetchNewsData();
+          await newsRepository.fetchNewsFromChannel(null);
+        },
+        child: Scaffold(
           backgroundColor: Colors.white,
-          leading: IconButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return CategoriesTabScreen();
-                  },
-                ));
-              },
-              icon: Icon(Icons.category_outlined)),
-          automaticallyImplyLeading: false,
-          centerTitle: true,
-          title: Text('HeadLines',
-              style: GoogleFonts.poppins(
-                  fontSize: 18, fontWeight: FontWeight.bold)),
-          actions: [
-            PopupMenuButton<PakistaniNewsChannel>(
-              onSelected: (PakistaniNewsChannel selectedChannel) {
-                newsRepository.fetchNewsFromChannel(selectedChannel);
-                print('selectedChannel:${selectedChannel}');
-              },
-              itemBuilder: (BuildContext context) {
-                return PakistaniNewsChannel.values
-                    .map((PakistaniNewsChannel channel) {
-                  return PopupMenuItem<PakistaniNewsChannel>(
-                    value: channel,
-                    child: Text(channel.displayName),
-                  );
-                }).toList();
-              },
-            ),
-          ],
-        ),
-        body: Obx(() {
-          // Show loading spinner when fetching news data
-          if (newsRepository.isLoading.value &&
-              newsRepository.countryArticles.isEmpty) {
-            return Center(
-                child: SpinKitCircle(size: 50.r, color: Colors.amber));
-          }
+          appBar: AppBar(
+            scrolledUnderElevation: 0,
+            elevation: 0,
+            backgroundColor: Colors.white,
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return CategoriesTabScreen();
+                    },
+                  ));
+                },
+                icon: Icon(Icons.category_outlined)),
+            automaticallyImplyLeading: false,
+            centerTitle: true,
+            title: Text('HeadLines',
+                style: GoogleFonts.poppins(
+                    fontSize: 18, fontWeight: FontWeight.bold)),
+            actions: [
+              PopupMenuButton<PakistaniNewsChannel>(
+                onSelected: (PakistaniNewsChannel selectedChannel) {
+                  newsRepository.fetchNewsFromChannel(selectedChannel);
+                  print('selectedChannel:${selectedChannel}');
+                },
+                itemBuilder: (BuildContext context) {
+                  return PakistaniNewsChannel.values
+                      .map((PakistaniNewsChannel channel) {
+                    return PopupMenuItem<PakistaniNewsChannel>(
+                      value: channel,
+                      child: Text(channel.displayName),
+                    );
+                  }).toList();
+                },
+              ),
+            ],
+          ),
+          body: Obx(() {
+            // Show loading spinner when fetching news data
+            if (newsRepository.isLoading.value &&
+                newsRepository.countryArticles.isEmpty) {
+              return Center(
+                  child: SpinKitCircle(size: 50.r, color: Colors.amber));
+            }
 
-          return SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  // ---------- Horizontal List (Country-based News) ----------
-                  SizedBox(
-                    height: 2.h,
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 15.w, vertical: 10.h),
-                    child: Text("Top Headlines",
-                        style: GoogleFonts.poppins(
-                            fontSize: 16, fontWeight: FontWeight.w400)),
-                  ),
-                  SizedBox(
-                    height: 280.h,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: newsRepository.countryArticles.length,
-                      itemBuilder: (context, index) {
-                        final article = newsRepository.countryArticles[index];
-                        return SizedBox(
-                          width: 340.w, // or any width you prefer
-                          child:
-                              buildNewsCard(article, index, isHorizontal: true),
-                        );
-                      },
+            return SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    // ---------- Horizontal List (Country-based News) ----------
+                    SizedBox(
+                      height: 2.h,
                     ),
-                  ),
-
-                  // ---------- Vertical List (Channel-based News) ----------
-                  if (newsRepository.channelArticles.isNotEmpty) ...[
                     Padding(
                       padding: EdgeInsets.symmetric(
                           horizontal: 15.w, vertical: 10.h),
-                      child: Text("Channel News",
+                      child: Text("Top Headlines",
                           style: GoogleFonts.poppins(
                               fontSize: 16, fontWeight: FontWeight.w400)),
                     ),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: newsRepository.channelArticles.length,
-                      itemBuilder: (context, index) {
-                        final article = newsRepository.channelArticles[index];
-                        return buildNewsCard(article, index,
-                            isHorizontal: false);
-                      },
-                    ),
-                  ] else ...[
-                    Center(
-                      child: Text(
-                        'Not to found data\nPlease check your internet connection',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.poppins(
-                            fontSize: 14.sp, color: Colors.black54),
+                    SizedBox(
+                      height: 280.h,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: newsRepository.countryArticles.length,
+                        itemBuilder: (context, index) {
+                          final article = newsRepository.countryArticles[index];
+                          return SizedBox(
+                            width: 340.w, // or any width you prefer
+                            child: buildNewsCard(article, index,
+                                isHorizontal: true),
+                          );
+                        },
                       ),
-                    )
-                  ]
-                ],
+                    ),
+
+                    // ---------- Vertical List (Channel-based News) ----------
+                    if (newsRepository.channelArticles.isNotEmpty) ...[
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 15.w, vertical: 10.h),
+                        child: Text("Channel News",
+                            style: GoogleFonts.poppins(
+                                fontSize: 16, fontWeight: FontWeight.w400)),
+                      ),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: newsRepository.channelArticles.length,
+                        itemBuilder: (context, index) {
+                          final article = newsRepository.channelArticles[index];
+                          return buildNewsCard(article, index,
+                              isHorizontal: false);
+                        },
+                      ),
+                    ] else ...[
+                      Center(
+                        child: Text(
+                          'Not to found data\nPlease check your internet connection',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.poppins(
+                              fontSize: 14.sp, color: Colors.black54),
+                        ),
+                      )
+                    ]
+                  ],
+                ),
               ),
-            ),
-          );
-        }),
+            );
+          }),
+        ),
       ),
     );
   }
